ed8baa9a8bed281984f4bf09947a4c243505ad47
bl0m1 <hugo.blom@live.se>
2013-12-06 18:54:32 +0100

    test

--- FireworkShop/src/com/alto/fireworkshop/fireworkshop.java
+++ FireworkShop/src/com/alto/fireworkshop/fireworkshop.java
@@ -7,7 +7,7 @@ public class fireworkshop extends JavaPlugin {
 	@Override
 	public void onDisable() {
 		System.out.println("[Warning] Disabled!");
-		System.out.println("Ost blom! massor av ost!");
+		System.out.println("[Warning] alto isnt op anymore");
 	}
 
 }
