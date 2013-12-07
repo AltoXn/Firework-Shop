c02eba9be1b989bd80de47668fe7e00b8018aabf
AltoXn <Alto@live.se>
2013-12-07 18:31:03 +0100

    Clean up

--- FireworkShop/src/com/alto/fireworkshop/fireworkshop.java
+++ FireworkShop/src/com/alto/fireworkshop/fireworkshop.java
@@ -1,12 +1,50 @@
 package com.alto.fireworkshop;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.CommandSender;
+import org.bukkit.ChatColor;
 
-public class fireworkshop extends JavaPlugin {
+import com.feildmaster.lib.configuration.PluginWrapper;
+import com.alto.fireworkshop.Commands;
+
+
+public class fireworkshop extends PluginWrapper {
+
+	private static fireworkshop plugin;
+	public static final String Permission = "firework.use";
+
+	@Override
+	public void onEnable() {
+		plugin = this;
+
+		new Commands().onEnable();
+
+		getCommand("firework").setExecutor(new Commands());
+
+		getServer().getPluginManager().registerEvents(new Commands(),this);
+
+		getLogger().info("Firework Plugin Enabled - Author: AltoXn");
+	}
 
 	@Override
 	public void onDisable() {
-		System.out.println("[Warning] Disabled! :) :P");
+		getServer().getScheduler().cancelTasks(this);
+		getLogger().info("Firework Plugin Disabled - Author: AltoXn");
 	}
 
+	public static void noPermission(CommandSender sender) {
+		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin
+				.getConfig().getString("no-permission-message")));
+	}
+
+	public static fireworkshop getInstance() {
+		return plugin;
+	}
+
+	public static void Usage(CommandSender sender, String cmd) {
+		sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+				"&cIncorrect command parameters."));
+		sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+				"&cUsage: " + cmd));
+	}
 }
+
--- FireworkShop/src/com/github/exloki/firework/Permissions.java
+++
@@ -1,6 +0,0 @@
-package com.github.exloki.firework;
-
-public class Permissions {
-
-	public static final String FIREWORK = "firework.use";
-}
